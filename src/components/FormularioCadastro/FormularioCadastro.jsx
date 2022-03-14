b789c4f1cffc9a9c8cb10bef8e9422295d7e7614
Antulio de Oliveira - SUPSE/SESEP/SECSD <ao@cin.ufpe.br>
2022-03-14 16:46:49 -0300

    alteracao para o resultado final

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,8 +7,21 @@ class FormularioCadastro extends Component {
     this.titulo ="";
     this.texto ="";
     this.categoria ="Sem Categoria";
+    this.state = {categorias:[]};
   }
 
+    componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+  }
+
+    componentWillUnmount (){
+     
+      this.props.categorias.desinscrever(this._novasCategorias.bind(this));
+    }
+    
+  _novasCategorias(categorias){
+   this.setState({...this.state, categorias})
+  }
   _handleMudancaCategoria(evento) {
     evento.stopPropagation();
     this.categoria = evento.target.value;
@@ -38,9 +51,9 @@ class FormularioCadastro extends Component {
       >
         <select onChange={this._handleMudancaCategoria.bind(this)} className="form-cadastro_input">
           <option> Sem Categoria</option>
-          {this.props.categorias.map((categoria =>{
-            return <option> {categoria} </option>
-          } ))}
+          {this.state.categorias.map((categoria,index) =>{
+            return <option key = {index} > {categoria} </option>
+          })}
 
         </select>
         <input
